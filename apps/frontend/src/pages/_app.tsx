e02698894385842c3ee23f0bb8d215e6b54b2925
Daniel Treviño Bergman <danielivert@gmail.com>
2022-01-29 20:11:58 +0100

    feat: move styles folder inside

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import '../../styles/globals.css'
+import '../styles/globals.css'
 import AppBootstrap from '../components/AppBootstrap'
 
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
