238e2a447ec6e265242dd3eed6339878337a3fcb
Daniel Treviño Bergman <danielivert@gmail.com>
2022-01-29 20:32:41 +0100

    fix: deploy hardhat to other networks

--- apps/hardhat-core/scripts/publish.ts
+++ apps/hardhat-core/scripts/publish.ts
@@ -1,60 +1,19 @@
+import * as dotenv from 'dotenv'
 import chalk from 'chalk'
 import fs from 'fs'
-import { folderExists, jsonStringify, paths } from './build-utils'
+import { folderExists, paths } from './build-utils'
+
+dotenv.config({ path: '../../../.env' })
 
 const deploymentsDir = './deployments'
 const publishDir = paths.EXPORTED_GENERATED_CONTRACTS_DIR
-const graphDir = '../subgraph'
-const graphAbisDir = `${graphDir}/abis`
-const graphConfigDirPath = `${graphDir}/config`
+const defaultNetwork = process.env.NEXT_PUBLIC_DEFAULT_NETWORK_NAME ?? 'hardhat'
 
-const publishContract = (contractName: string, networkNameDirectory: string): boolean => {
-  console.log(' 💽 Publishing', chalk.cyan(contractName), 'to', chalk.gray(publishDir))
-  try {
-    const contractString = fs
-      .readFileSync(`${deploymentsDir}/${networkNameDirectory}/${contractName}.json`)
-      .toString()
-    const contractObject = JSON.parse(contractString)
-    const graphConfigPath = `${graphConfigDirPath}/config.json`
-    let graphConfig = '{}'
-    try {
-      if (fs.existsSync(graphConfigPath)) {
-        graphConfig = fs.readFileSync(graphConfigPath).toString()
-      }
-    } catch (e) {
-      console.log(e)
-    }
-
-    const graphConfigObject = JSON.parse(graphConfig)
-    graphConfigObject[`${networkNameDirectory}_${contractName}Address`] = contractObject.address
-    // Make sure {graphDir}/config exists
-    folderExists(graphConfigDirPath)
-    fs.writeFileSync(graphConfigPath, jsonStringify(graphConfigObject))
-
-    // Make sure {graphDir}/abis exists
-    folderExists(graphAbisDir)
-    fs.writeFileSync(
-      `${graphDir}/abis/${networkNameDirectory}_${contractName}.json`,
-      jsonStringify(contractObject)
-    )
-
-    console.log(`🛸 Published ${chalk.green(contractName)} to the subgraph.`)
-
-    return true
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    if (e.toString().indexOf('no such file or directory') >= 0) {
-      console.log(
-        chalk.yellow(` ⚠️  Can't publish ${contractName} yet (make sure it getting deployed).`)
-      )
-      console.error(e)
-    } else {
-      console.log(e)
-      return false
-    }
+const publishContract = (contractName: string, networkNameDirectory: string): void => {
+  if (networkNameDirectory === defaultNetwork) {
+    console.log('💽 Publishing', chalk.cyan(contractName), 'to', chalk.gray(publishDir))
+    console.log(`🛸 Deployed contract to: ${chalk.magenta(defaultNetwork)}`)
   }
-
-  return false
 }
 
 const init = async (): Promise<void> => {
@@ -70,7 +29,6 @@ const init = async (): Promise<void> => {
       }
     })
   })
-  console.log('✅  Published contracts to the subgraph package.')
 }
 
 init()
